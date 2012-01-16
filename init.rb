4ac198d73e77a19a9f3c0f12efceae98d4991991
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2012-01-16 19:27:01 +0100

    re-added init.rb

--- init.rb
+++ init.rb
@@ -23,12 +23,45 @@
 # NOTE: Variables set like this are exported in the generated 'env.sh' script.
 #
 
-Autoproj.root_dir()
-
 require 'autoproj/gitorious'
 Autoproj.gitorious_server_configuration('GITORIOUS', 'gitorious.org')
 Autoproj.gitorious_server_configuration('SPACEGIT', 'spacegit.dfki.uni-bremen.de')
 
-
 Autoproj.env_inherit 'CMAKE_PREFIX_PATH'
 Autoproj.shell_helpers = false
+
+
+    configuration_option 'ROCK_FLAVOR', 'string',
+    :default => 'master',
+    :values => ['stable', 'next', 'master'],
+    :doc => [
+        "TEST"]
+
+    Autoproj.user_config('ROCK_FLAVOR')
+
+#enable ccache 
+#todo options at bootstrap
+#    env_set 'CC',"/usr/lib/ccache/gcc"
+#    env_set 'CXX',"/usr/lib/ccache/g++"###
+#
+#    env_set 'CMAKE_CXX_COMPILER_ARG1',"-m32"
+#    env_set 'CMAKE_C_COMPILER_ARG1',"-m32"#
+##
+#
+#    env_set 'CCACHE_PREFIX',"distcc"
+#    env_set 'DISTCC_HOSTS',"CoHoN-3-u"
+
+    #enable ccache
+    Autobuild.parallel_build_level = 20
+
+    env_set 'CC',"/usr/lib/ccache/gcc"
+    env_set 'CXX',"/usr/lib/ccache/g++"
+
+    env_set 'CCACHE_PREFIX',"distcc"
+    #env_set 'DISTCC_HOSTS',"'localhost CoHoN-3-u aburchardt-u gaudig aduda-u uwdesktop fritsche'"
+    #env_set 'DISTCC_HOSTS',"\"localhost CoHoN-3-u gaudig\""
+
+    env_set 'CXXFLAGS',"-m32"
+    env_set 'CFLAGS',"-m32"
+
+
