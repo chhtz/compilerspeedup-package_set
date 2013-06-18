ebae329a9a9d55560f18e186f8c9888301941f75
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2013-06-18 09:57:12 +0200

    init iceCC without ICECC_VERSION

--- init.rb
+++ init.rb
@@ -36,119 +36,30 @@ Autoproj.env_inherit 'CMAKE_PREFIX_PATH'
 #use more options "wrapper path", ccache only
 #option to replace /etc/default/distcc
 
-#enable ccache
-    configuration_option 'CCache', 'string',
-    :default => 'no',
-    :values => ['yes', 'no'],
-    :doc => ["Enable ccache?",
-             "CCache chaches your preprocessor output and according binaries.",
-             "It can speed up recompiling",
-             "To clear the buffer call \"ccache -C\"",
-             "Do you want to use ccache for compiling sources [yes/no]"]
-
-    Autoproj.user_config('CCache')
 
 #enable distcc
-    configuration_option 'DistCC', 'string',
+    configuration_option 'iceCC', 'string',
     :default => 'no',
     :values => ['yes', 'no'],
-    :doc => ["Enable distcc?",
+    :doc => ["Enable icecc?",
              "Distcc compiles distributed in the Network.",
              "If you have a slow machine, this speeds up compiling",
-             "Do you want to use distcc for compiling sources [yes/no]"]
+             "Do you want to use icecc for compiling sources [yes/no]"]
 
-    Autoproj.user_config('DistCC')
+    Autoproj.user_config('iceCC')
 
 
 
 #the actural settings
 
-if (Autoproj.user_config('CCache') == 'yes') then
-    env_set 'CC',"/usr/lib/ccache/gcc"
-    env_set 'CXX',"/usr/lib/ccache/g++"
 
-end
-
-
-if (Autoproj.user_config('DistCC') == 'yes') then
-	
-    if (Autoproj.user_config('CCache') == 'yes') then
-    	env_set 'CCACHE_PREFIX',"distcc"
-    else
-        env_set 'CC',"/usr/lib/distcc/gcc-4.4"
-        env_set 'CXX',"/usr/lib/distcc/g++-4.4"
+if (Autoproj.user_config('iceCC') == 'yes') then
+        env_set 'PATH=/usr/lib/icecc/bin:$PATH'
     end
-    
-    #env_set 'DISTCC_HOSTS',"'localhost CoHoN-3-u aburchardt-u gaudig aduda-u uwdesktop fritsche'"
-    #env_set 'DISTCC_HOSTS',"\"localhost CoHoN-3-u gaudig\""
-
-
-#   configuration_option 'DistCCArch', 'string',
-#    :default => '32',
-#    :values => ['32', '64'],
-#    :doc => ["Which Architecture do you use? [gcc opttion for -m like [32,64]]"]
-#    Autoproj.user_config('DistCCArch')
-#    env_set 'CXXFLAGS',"-m" + Autoproj.user_config('DistCCArch')
-#    env_set 'CFLAGS',"-m" + Autoproj.user_config('DistCCArch')
-
-#   configuration_option 'DistCCBuildLevel', 'fixnum',
-#    :default => '20',
-#    :values => [],
-#    :doc => ["How many parallel builds should be preferred?"]
-#    Autoproj.user_config('DistCCBuildLevel')
-#
-#    Autobuild.parallel_build_level = Autoproj.user_config('DistCCBuildLevel')
-    Autobuild.parallel_build_level = 30
-
-   configuration_option( 'useDistCCDir', 'string',
-    :default => 'yes',
-    :values => ['yes','no'],
-    :doc => [   "Use DFKI distcc Server List?",
-                "There is a list of DFKI distcc servers.",
-                "The servers are checked for availability on sourcing env.sh",
-                "(the DFKI host list needs the package 'distcc_conf')",
-                 "Add this to your manifest:",
-                 "layout:\n    - external:\n        - distcc_conf",
-                "Should the DFKI host list be used?",
-                "(if no, you need to set up /etc/distcc/hosts manually) ? [yes/no]"])
-    Autoproj.user_config('useDistCCDir')
-
-	if (Autoproj.user_config('useDistCCDir') == 'yes') then
-        configuration_option 'DistCCDir', 'string',
-        :default => Autoproj.root_dir() + '/external/distcc_conf',
-        :values => [],
-        :doc => ["Set the location of the distcc_conf package",
-                 "(where the hosts file and updatedistcchosts.sh is located)"]
-        Autoproj.user_config('DistCCDir')
-    
-	env_set 'DISTCC_DIR', Autoproj.user_config('DistCCDir')
-        env_set 'CC',Autoproj.user_config('DistCCDir') + "/ccache_gcc"
-        env_set 'CXX',Autoproj.user_config('DistCCDir') + "/ccache_g++"
-	env_set 'CCACHE_PREFIX',"distcc"
-	
-        Autobuild.env_source_file(Autoproj.user_config('DistCCDir') + "/updatedistcchosts.sh")
-
-    end
-	
-#	env_set 'DISTCC_HOSTS','"localhost gaudig"'
+	puts("You need to run source env.sh before changes take effect")
+    Autobuild.parallel_build_level = 10
 
 else
-    puts("You need to restart the console and source env.sh before changes take effect")
-#    STDIN.readline
-#    system("unset CXXFLAGS")
-#    system("unset CFLAGS")
-    #env_set 'CXXFLAGS',""
-    #env_set 'CFLAGS',""	
-    
+    puts("You need to restart the console and source env.sh before changes take effect")    
 end	
-
-if (Autoproj.user_config('DistCC') == 'no' && Autoproj.user_config('CCache') == 'no') then
-    puts("You need to restart the console and source env.sh before changes take effect")
-#    STDIN.readline
-#    system("unset CXX")
-#    system("unset CC")
-#    env_set 'CXX',""
-#    env_set 'CC',""	
-end
-
 
